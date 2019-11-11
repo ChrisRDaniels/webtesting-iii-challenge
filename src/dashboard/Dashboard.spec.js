a70b179d9163b88a7a20d2ef818494c4e8a68c84
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-11-11 15:40:29 -0700

    render and default gate tests working

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import Dashboard from './Dashboard';
+
 // Test away
+
+test('App renders successfully', () => {
+    render(<Dashboard />)
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import Display from './Display';
+
 // Test away!
+
+test('Defaults to open and unlocked', () => {
+    const { getByText } = render(<Display />)
+
+    getByText (/^unlocked$/i)
+    getByText (/^open$/i)
+})
